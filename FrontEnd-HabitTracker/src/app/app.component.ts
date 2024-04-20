cd51d92be79fa692a9238efe3250b7fc374d03fd
PiotrSDAZDJAVApol143 <xav88er@gmail.com>
2024-04-20 22:19:21 +0200

    Angular - register and errors interceptor

--- FrontEnd-HabitTracker/src/app/app.component.ts
+++ FrontEnd-HabitTracker/src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NotifierService} from "angular-notifier";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'FrontEnd-HabitTracker';
+
+  constructor(private notifierService: NotifierService) {
+  }
+
+  ngOnInit(): void {
+    this.notifierService.notify('Success', 'Operation success!');
+  }
 }
--- FrontEnd-HabitTracker/src/app/app.module.ts
+++ FrontEnd-HabitTracker/src/app/app.module.ts
@@ -4,19 +4,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { CoreModule } from './modules/core/core.module';
+import { EffectsModule } from '@ngrx/effects';
 import { AuthModule } from './modules/auth/auth.module';
-import {CoreModule} from "./modules/core/core.module";
+import { authReducer } from './modules/auth/store/auth.reducer';
+import { NotifierModule, NotifierOptions } from 'angular-notifier';
+import { AuthEffects } from './modules/auth/store/auth.effects';
+
+const customNotifier: NotifierOptions = {
+  position: {
+    horizontal: {
+      position: 'right',
+      distance: 12,
+    },
+    vertical: {
+      position: 'top',
+      distance: 12,
+      gap: 10,
+    },
+  },
+  theme: 'material',
+};
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CoreModule,
     AuthModule,
     BrowserAnimationsModule,
-    CoreModule,
+    StoreModule.forRoot({ auth: authReducer }),
+    EffectsModule.forRoot([AuthEffects]),
+    NotifierModule.withConfig(customNotifier),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
